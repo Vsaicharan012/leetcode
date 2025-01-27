2aefa3636461ec1d670976215b214278859cd458
Vsaicharan012 <saicharanveeramalla0@gmail.com>
2025-01-27 03:24:47 -0500

    Time: 2 ms (63.42%), Space: 41.6 MB (99.4%) - LeetHub

---
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        // Dynamic programming approach
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // Base case: empty string matches empty pattern
+        dp[0][0] = true;
+
+        // Handle patterns like a*, a*b*, etc.
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
